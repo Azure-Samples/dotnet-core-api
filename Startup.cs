5a3706d634f340d8a712ce741269efc83ee187c6
Bruno Marin <bruno_marin@hotmail.com>
2020-01-08 11:19:54 -0300

    Add authentication to Startup

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using TodoApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace TodoApi
 {
     public class Startup
-    {       
+    {
+        public IConfiguration Configuration { get; }
+        
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
@@ -16,8 +33,10 @@ namespace TodoApi
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "Sample dotnet Core API", Version = "v1" });
             });
+            
+            ConfigureAuthentication(services);
         }
 
         public void Configure(IApplicationBuilder app)
@@ -28,13 +47,78 @@ namespace TodoApi
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample dotnet Core API");
             });
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseMvc();
+        }
+
+        private void ConfigureAuthentication(IServiceCollection services)
+        {
+            services.AddAuthentication(authOptions =>
+            {
+                authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwtOptions =>
+            {
+                jwtOptions.Authority = Configuration["Authentication:Authority"];
+
+                jwtOptions.SaveToken = false;
+                jwtOptions.IncludeErrorDetails = true;
+
+                if (Environment.IsDevelopment())
+                {
+                    jwtOptions.RequireHttpsMetadata = false;
+                }
+
+                jwtOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidIssuer = Configuration["Authentication:Authority"],
+                    ValidateLifetime = true
+                };
+
+                jwtOptions.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = failedAuthContext =>
+                    {
+                        failedAuthContext.NoResult();
+                        failedAuthContext.Response.StatusCode = 500;
+                        failedAuthContext.Response.ContentType = "text/plain";
+
+                        var errorMessage = failedAuthContext.Exception switch
+                        {
+                            SecurityTokenExpiredException _ => "Expired token.",
+                            SecurityTokenNotYetValidException _ => "Token not yet valid.",
+                            SecurityTokenInvalidLifetimeException _ => "Invalid token lifetime.",
+                            SecurityTokenNoExpirationException _ => "Missing token expiration.",
+                            SecurityTokenSignatureKeyNotFoundException _ => "Invalid token. Key not found.",
+                            _ => "An error occured processing your authentication."
+                        };
+
+                        return failedAuthContext.Response.WriteAsync(errorMessage);
+                    },
+                    
+                    // OnMessageReceived = debugContext =>
+                    // {
+                    //     var response = debugContext.Response;
+                    //     return Task.CompletedTask;
+                    // }
+                };
+            });
+
+            services.AddAuthorization(policyOptions =>
+            {
+                policyOptions.AddPolicy("Administrator", new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .Build());
+            });
         }
     }
 }
